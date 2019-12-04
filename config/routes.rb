3e7233dc58628cfd87c2f27cff53a46b7d451a22
benoit bessiere <benoit.bessiere@gmail.com>
2019-12-04 23:38:37 +0100

    add navbar + footer + flip card

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  get 'flip', to: 'pages#flip', as: :flip
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
