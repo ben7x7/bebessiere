61849f90694068428c0a2c758d8153ee7916faaf
benoit bessiere <benoit.bessiere@gmail.com>
2019-12-16 14:40:36 +0100

    add components

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get 'flip', to: 'pages#flip', as: :flip
   get 'collections', to: 'pages#collections', as: :collections
+  get 'animations', to: 'pages#animations', as: :animations
+  get 'images', to: 'pages#images', as: :images
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
