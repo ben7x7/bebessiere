aa52f5da768899dab01cb67494ac7ee7dc703826
benoit bessiere <benoit.bessiere@gmail.com>
2020-02-29 15:33:19 +0100

    update gem puma

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'collections', to: 'pages#collections', as: :collections
   get 'animations', to: 'pages#animations', as: :animations
   get 'images', to: 'pages#images', as: :images
+  get 'sample404', to: 'pages#sample404', as: :sample404
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
